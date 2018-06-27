f1921054e011e2e11e6d07fe061e6e8ef05beed2
e <lambert1129@foxmail.com>
2018-06-27 12:09:47 +0800

    完成技能项目的增减

--- src/js/root.js
+++ src/js/root.js
@@ -28,13 +28,13 @@ const app = new Vue({
                 content:'我是来自山东的xxx，大家所看到的我的内在就像我的外表一样，敦厚和实在是我对自己最好的概括。专业扎实，看书是我最大的享受，钻研电脑让我感觉其乐无穷。我做事踏实，再小的事情我也要一丝不苟地完成。给我一个机会，我会给您一个惊喜。'
             },
             projects:[
-                {name:'无缝轮播',skill:'JavaScript、jQuery',link:'http://www.baidu.com',describe:'该无缝轮播能够自动播放、点击前后按钮切换图片、点击第N个灰色按钮切换到第N张图片。通过百分比布局实现更改图片尺寸不影响无缝轮播效果，通过jQuery动态生成与img数量相等的button，通过jQuery动态增删class来激活button的状态。'},
-                {name:'无缝轮播',skill:'JavaScript、jQuery',link:'http://www.baidu.com',describe:'该无缝轮播能够自动播放、点击前后按钮切换图片、点击第N个灰色按钮切换到第N张图片。通过百分比布局实现更改图片尺寸不影响无缝轮播效果，通过jQuery动态生成与img数量相等的button，通过jQuery动态增删class来激活button的状态。'},
-                {name:'无缝轮播',skill:'JavaScript、jQuery',link:'http://www.baidu.com',describe:'该无缝轮播能够自动播放、点击前后按钮切换图片、点击第N个灰色按钮切换到第N张图片。通过百分比布局实现更改图片尺寸不影响无缝轮播效果，通过jQuery动态生成与img数量相等的button，通过jQuery动态增删class来激活button的状态。'},
+                {name:'无缝轮播',useSkill:'JavaScript、jQuery',link:'http://www.baidu.com',describe:'该无缝轮播能够自动播放、点击前后按钮切换图片、点击第N个灰色按钮切换到第N张图片。通过百分比布局实现更改图片尺寸不影响无缝轮播效果，通过jQuery动态生成与img数量相等的button，通过jQuery动态增删class来激活button的状态。'},
+                {name:'无缝轮播',useSkill:'JavaScript、jQuery',link:'http://www.baidu.com',describe:'该无缝轮播能够自动播放、点击前后按钮切换图片、点击第N个灰色按钮切换到第N张图片。通过百分比布局实现更改图片尺寸不影响无缝轮播效果，通过jQuery动态生成与img数量相等的button，通过jQuery动态增删class来激活button的状态。'},
+                {name:'无缝轮播',useSkill:'JavaScript、jQuery',link:'http://www.baidu.com',describe:'该无缝轮播能够自动播放、点击前后按钮切换图片、点击第N个灰色按钮切换到第N张图片。通过百分比布局实现更改图片尺寸不影响无缝轮播效果，通过jQuery动态生成与img数量相等的button，通过jQuery动态增删class来激活button的状态。'},
                 
             ],
             skills:[
-                {name:'Html',describe:'完美还原设计稿'},
+                {name:'Html、CSS、javascript',describe:'完美还原设计稿完美还原设计稿完美还原设计稿完美还原设计稿完美还原设计稿完美还原设计稿完美还原设计稿完美还原设计稿'},
                 {name:'jQuery',describe:'完美还原设计稿'},
                 {name:'CSS',describe:'完美还原设计稿'},
                 {name:'javascript',describe:'完美还原设计稿'},              
@@ -50,6 +50,24 @@ const app = new Vue({
             stateself:'自我介绍 / STATEMENT',
             projects:'项目经历 / PROJECT EXPERIENCE',
             skills:'技能 / SKILLS',
+        }
+    },
+    methods:{
+        addProject(){
+            this.resume.projects.push(
+                {name:'无缝轮播',useSkill:'JavaScript、jQuery',link:'http://www.baidu.com',describe:'该无缝轮播能够自动播放、点击前后按钮切换图片、点击第N个灰色按钮切换到第N张图片。通过百分比布局实现更改图片尺寸不影响无缝轮播效果，通过jQuery动态生成与img数量相等的button，通过jQuery动态增删class来激活button的状态。'},                
+            )           
+        },
+        removeProject(index){
+            this.resume.projects.splice(index,1)
+        },
+        addSkill(){
+            this.resume.skills.push(
+                {name:'jQuery',describe:'完美还原设计稿'},               
+            )
+        },
+        removeSkill(index){
+            this.resume.skills.splice(index,1)            
         }
     }
 })
